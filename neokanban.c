8fcdded98edbd336b93e225d0f223bd914983bbf
ssh1nrr <147348911+ssh1nrr@users.noreply.github.com>
2024-10-26 16:45:15 -0300

    build and print clean table when the file is empty

--- neokanban.c
+++ neokanban.c
@@ -10,7 +10,8 @@
 
 void print_table(char* table[HEIGHT][N_COLS]);
 void print_separator(void);
-void print_cell(int col_id, char* content);
+void print_header(void);
+void print_cell(size_t col_id, char* content);
 
 const size_t TODO = 0;
 const size_t DOING = 1;
@@ -36,18 +37,37 @@ int main(int argc, char* argv[])
 	char *table[HEIGHT][N_COLS];
 
 	FILE *data = fopen(data_file, "wb+");
+	bool empty = true;
 
 	Column cols[N_COLS];
 
 	Column *buf = malloc(sizeof(Column));
-	while(fread(buf, sizeof(buf), 1, data))
+	while(fread(buf, sizeof(Column), 1, data))
 	{
+		empty = false;
 		for (int i = 0; i < buf->populated; i++)
 		{
-			table[i][buf->id] = buf->tasks[i].content;
+			table[i][buf->id] = strdup(buf->tasks[i].content);
+		}
+	}
+
+	free(buf);
+	fclose(data);
+
+	if (empty)
+	{
+		for (int i = 0; i < HEIGHT; i++)
+		{
+			for (int j = 0; j < N_COLS; j++)
+			{
+				table[i][j] = malloc(sizeof(char) * MAX_BUF);
+				snprintf(table[i][j], MAX_BUF, "");
+				table[i][j][strlen(table[i][j])] = '\0';
+			}
 		}
 	}
 	print_table(table);
+
 	//
 	// for (int i = 0; i < N_COLS; i++)
 	// {
@@ -60,7 +80,7 @@ int main(int argc, char* argv[])
 
 void print_separator(void)
 {
-	int full_width = COL_WIDTH * N_COLS;
+	size_t full_width = COL_WIDTH * N_COLS;
 	for (int i = 0; i < full_width; i++)
 	{
 		if (i == 0 || i == full_width - 1)
@@ -75,7 +95,7 @@ void print_separator(void)
 	printf("\n");
 }
 
-void print_table(char* table[HEIGHT][N_COLS])
+void print_header(void)
 {
 	print_separator();
 	char* titles[] = {"to-do", "doing", "done"};
@@ -83,21 +103,32 @@ void print_table(char* table[HEIGHT][N_COLS])
 	{
 		print_cell(i, titles[i]);
 	}
-	printf("\n");
 	print_separator();
 }
 
-void print_cell(int col_index, char* content)
+void print_table(char* table[HEIGHT][N_COLS])
 {
-	int	n_spaces;
+	print_header();
+	for (int i = 0; i < HEIGHT; i++)
+	{
+		for (int j = 0; j < N_COLS; j++)
+		{
+			print_cell(j, table[i][j]);
+		}
+	}
+	print_separator();
+}
+
+void print_cell(size_t col_index, char* content)
+{
+	size_t n_spaces;
 	if (col_index == 0)
 	{
-		printf("| ");
+		printf("|");
 		n_spaces = COL_WIDTH - strlen(content) - 3;
 	}
 	else
 	{
-		printf(" ");
 		n_spaces = COL_WIDTH - strlen(content) - 2;
 	}
 
@@ -105,14 +136,19 @@ void print_cell(int col_index, char* content)
 	int len = strlen(content);
 	if (len < COL_WIDTH - 2)
 	{
-		printf("%s", content);
+		printf(" %s", content);
 		for (int i = 0; i < n_spaces; i++)
 		{
 				printf(" ");
 		}
 		printf("|");
-		return;
 	}
-	
 	// TODO prevent bigger strings of overflowing
+	// 
+	//
+	// last col adds a newline
+	if (col_index == N_COLS - 1)
+	{
+		printf("\n");
+	}
 }
